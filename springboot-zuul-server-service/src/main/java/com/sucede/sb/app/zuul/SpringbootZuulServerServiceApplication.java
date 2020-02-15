9bcf59c0fbf2c9883a4728155404e2d8b048221a
Daniel Fernandez <dfernandez@certiorem.com>
2020-02-15 06:44:57 +0100

    Servicio zuul server

---
+++ springboot-zuul-server-service/src/main/java/com/sucede/sb/app/zuul/SpringbootZuulServerServiceApplication.java
@@ -0,0 +1,17 @@
+package com.sucede.sb.app.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class SpringbootZuulServerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootZuulServerServiceApplication.class, args);
+	}
+
+}
---
+++ springboot-zuul-server-service/src/test/java/com/sucede/sb/app/zuul/SpringbootZuulServerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sucede.sb.app.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootZuulServerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
